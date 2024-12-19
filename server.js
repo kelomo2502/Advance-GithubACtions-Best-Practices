a4c9aca1c7a579267c0e59886613c202f8831e19
oyewunmi gbenga kelvin <kelvinoye@gmail.com>
2024-12-19 07:15:23 +0100

    Add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Simple endpoint to return a welcome message
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js Server!');
+});
+
+// Another endpoint for testing
+app.get('/status', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
